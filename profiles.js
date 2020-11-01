a6a4f8771ae770ccd71a3e30fe9f5d6f73c45a55
Sarah Tackett <sarahtackett@gmail.com>
2020-10-31 20:51:04 -0400

    BugFix for Woe and Orchil

--- profiles.js
+++ profiles.js
@@ -1,16 +1,122 @@
 "use strict";
-// orchil/profiles.js
 var profiles = {
-        "portal_gables":{
-                "method":   "websocket",
-                "protocol": "ws",
-                "server":   "localhost", //"chat.gables.chattheatre.com",
-                "port":      10800,
-                "http_port": 10080,
-                "woe_port":  10802,
-                "path":     "/gables",
-                "extra":    "",
-                "reports":   false,
-                "chars":    true,
-        }
+	"tectesttunnel": {
+		"method":   "websocket",
+		"protocol": "ws",
+		"server":   "tectest.skotos.net",
+		"port":      8867,
+		"path":     "/orchil/",
+		"extra":    "tec"
+	},
+	"tectest": {
+		"method":   "websocket",
+		"protocol": "ws",
+		"server":   "tectest.skotos.net",
+		"port":      7707,
+		"path":     "/orchil/foobar",
+		"extra":    "tec"
+	},
+	"grtunnel": {
+		"method":   "websocket",
+		"protocol": "ws",
+		"server":   "gr.skotos.net",
+		"port":      2080,
+		"path":     "/orchil/",
+		"extra":    "gr"
+	},
+	"popcornajax": {
+		"method":   "ajax",
+		"protocol": "https",
+		"server":   "192.168.1.67",
+		"port":      443,
+		"path":     "/orchil/server.php",
+		"rate":     1,
+		"extra":    "gr"
+	},
+	"portal_tectest":{
+		"method":   "websocket",
+		"protocol": "ws",
+		"server":   "skotos.net",
+		"port":      8080,
+		"path":     "/tectest",
+		"extra":    "tec",
+		"reports":  true,
+		"chars":    false
+	},
+	"portal_tec":{
+		"method":   "websocket",
+		"protocol": "ws",
+		"server":   "skotos.net",
+		"port":      8080,
+		"path":     "/tec",
+		"extra":    "tec",
+		"reports":  true,
+		"chars":    false
+	},
+	"portal_marrach":{
+		"method":   "websocket",
+		"protocol": "ws",
+		"server":   "skotos.net",
+		"port":      8080,
+		"woe_port":  8084,
+		"path":     "/marrach",
+		"extra":    "marrach",
+		"reports":   false,
+		"chars":     true,
+		"prompt":   "> "
+	},
+	"portal_grendelsrevenge":{
+		"method":   "websocket",
+		"protocol": "ws",
+		"server":   "skotos.net",
+		"port":      8080,
+		"path":     "/grendel",
+		"extra":    "gr",
+		"reports":  false,
+		"chars":    false
+	},
+	"portal_lovecraft":{
+		"method":   "websocket",
+		"protocol": "ws",
+		"server":   "skotos.net",
+		"port":      8080,
+		"woe_port":  8082,
+		"path":     "/lovecraft",
+		"extra":    "lcabn",
+		"reports":  false,
+		"chars":    true
+	},
+	"portal_ironclaw":{
+		"method":   "websocket",
+		"protocol": "ws",
+		"server":   "skotos.net",
+		"port":      8080,
+		"woe_port":  8083,
+		"path":     "/ironclaw",
+		"extra":    "ironclaw",
+		"reports":  false,
+		"chars":    true,
+	},	
+    "portal_allegory":{
+		"method":   "websocket",
+		"protocol": "ws",
+		"server":   "skotos.net",
+		"port":      8080,
+		"woe_port":  8083,
+		"path":     "/ironclaw",
+		"extra":    "ironclaw",
+		"reports":  false,
+		"chars":    true,
+	},
+	"portal_lazarus":{
+	"method":   "websocket",
+		"protocol": "ws",
+		"server":   "skotos.net",
+		"port":      8080,
+		"woe_port":  8081,
+		"path":     "/lazarus",
+		"extra":    "lazarus",
+		"reports":   false,
+		"chars":     true
+	}
 };
